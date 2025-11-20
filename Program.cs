9c72de3bb057c13f4d19a7e1ea297ffd7bf25a70
Opilane TTHK <opilane@tthk.ee>
2025-11-20 11:42:27 +0200

    da'õ

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kolmnurk");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= n)
+            {
+                Console.WriteLine();
+                int j = 1;
+                while (j <= i)
+                {
+                    Console.Write("*" + " ");
+                    j++;
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
